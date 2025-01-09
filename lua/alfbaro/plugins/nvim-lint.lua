eea26b5a6622eb8238ffec63dd2df88d6b5f3227
Alfredo Baez Rojas <carlos.baez@pucp.edu.pe>
2025-01-09 12:17:53 -0500

    Add check for swiftlint file

--- lua/alfbaro/plugins/nvim-lint.lua
+++ lua/alfbaro/plugins/nvim-lint.lua
@@ -11,12 +11,28 @@ return {
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+        local function file_exists_in_root(filename)
+            local root = vim.fn.getcwd()
+            local filepath = root .. "/" .. filename
+            return vim.fn.filereadable(filepath) == 1
+        end
+
+        local function try_lint_with_validation()
+            local ft = vim.bo.filetype
+            if ft == "swift" and not file_exists_in_root(".swiftlint.yml") then
+                vim.notify(
+                    "Swiftlint configuration file not found in project root. Skipping Swift linting",
+                    vim.log.levels.INFO
+                )
+                return
+            end
+            lint.try_lint()
+        end
+
         -- this can have another value "TextChanged"
         vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
             group = lint_augroup,
-            callback = function()
-                require("lint").try_lint()
-            end,
+            callback = try_lint_with_validation,
         })
 
         vim.keymap.set("n", "<leader>ml", function()
